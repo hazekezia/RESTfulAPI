b4417dd8db9e66f12b30e629c3b7bac5a9e73dca
Jonathan Sitohang <hazekezia@gmail.com>
2021-09-01 14:37:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ from app import app
 from config import mysql
 from flask import jsonify
 from flask import flash, request
-		
+
+# GET		
 @app.route('/api')
 def api():
 	try:
-		conn = mysql.connect()
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT id, name, harga FROM api_test")
-		empRows = cursor.fetchall()
-		respone = jsonify(empRows)
+		connect = mysql.connect()
+		cursor = connect.cursor(pymysql.cursors.DictCursor)
+
+		cursor.execute("SELECT id,name,harga FROM api_test")
+
+		api_result = cursor.fetchall()
+		respone = jsonify(api_result)
 		respone.status_code = 200
 		return respone
 	except Exception as e:
 		print(e)
 	finally:
 		cursor.close() 
-		conn.close()
+		connect.close()
 
 if __name__ == "__main__":
     app.run()
