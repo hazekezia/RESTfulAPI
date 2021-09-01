c1b50c2a1aed4797b8756a0e4da81b1cdf45e1e8
Jonathan Sitohang <hazekezia@gmail.com>
2021-09-01 14:12:26 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from app import app
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'hazekezia'
+mysql.init_app(app)
