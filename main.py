2ef474582ef348c9c287894c0f02ee0fd2891e74
Jonathan Sitohang <hazekezia@gmail.com>
2021-09-24 20:26:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ cursor = sql_connect.cursor(pymysql.cursors.DictCursor)
 # GET - Read Data
 @app.route("/api")
 def get():
-	cursor.execute("SELECT * FROM api_test")
+	cursor.execute("SELECT * FROM api1")
 
 	api_result = cursor.fetchall()
 	api_response = jsonify(api_result)
@@ -19,9 +19,18 @@ def get():
 
 @app.route("/api/<int:id>")
 def getid(id):
-	cursor.execute("SELECT * FROM api_test WHERE id =%s", id)
+	cursor.execute("SELECT * FROM api1 WHERE id =%s", id)
 	
 	api_result = cursor.fetchone()
+	api_response = jsonify(api_result)
+	api_response.status_code = 200
+	return api_response
+
+@app.route("/api2")
+def getapi2():
+	cursor.execute("SELECT * FROM api2")
+
+	api_result = cursor.fetchall()
 	api_response = jsonify(api_result)
 	api_response.status_code = 200
 	return api_response
@@ -30,19 +39,25 @@ def getid(id):
 @app.route("/api/create", methods=["POST"])
 def create():
 	request_json = request.json
-	counter = len(request_json)
-	print(counter)
 
 	if request.method == "POST":
-		for i in range(counter):
-			name = request_json[i]["nama"]
-			umur = request_json[i]["umur"]
+		name = request_json["nama"]
+		umur = request_json["umur"]
 
-			SQLCommand = "INSERT INTO api_test(nama, umur) VALUES(%s, %s)"
-			Values = (name, umur)
+		for item in request_json["items"]:
+			id_api1 = item["id_api1"]
+			desc = item["deskripsi"]
+			SQLCommand = "INSERT INTO api2(id_api1, deskripsi) VALUES(%s, %s)"
+			Values = (id_api1, desc)
 
 			cursor.execute(SQLCommand, Values)
 			sql_connect.commit()
+
+		SQLCommand = "INSERT INTO api1(nama, umur) VALUES(%s, %s)"
+		Values = (name, umur)
+
+		cursor.execute(SQLCommand, Values)
+		sql_connect.commit()
 
 		message = 	{
 					"status":"S",
@@ -64,7 +79,7 @@ def update():
 	umur = request_json["umur"]
 
 	if request.method == "PUT":
-		SQLCommand = "UPDATE api_test SET nama=%s, umur=%s WHERE id=%s"
+		SQLCommand = "UPDATE api1 SET nama=%s, umur=%s WHERE id=%s"
 		Values = (nama, umur, id)
 
 		cursor.execute(SQLCommand, Values)
@@ -84,7 +99,7 @@ def update():
 # DELETE - Delete Data
 @app.route("/api/delete/<int:id>", methods=["DELETE"])
 def delete(id):
-	cursor.execute("DELETE FROM api_test WHERE id =%s", (id,))
+	cursor.execute("DELETE FROM api1 WHERE id =%s", (id,))
 	sql_connect.commit()
 
 	message = {
@@ -111,4 +126,3 @@ app.debug = True
 
 if __name__ == "__main__":
     app.run(threaded=True)
-	
